6f4a25db13381051349660798c5b93c48f2ba0a0
Pratheek Rebala <pratheekrebala@gmail.com>
2017-02-12 13:01:32 -0500

    Update api wrapper
    Clean up the api calls and allow for file downloads
    
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- fcc-api/api.py
+++ fcc-api/api.py
@@ -1,32 +1,35 @@
 import requests
 import requests_cache
 import simplejson as json
+from urllib.request import urlretrieve
+import urllib.parse as urlparse
+import os
 
-requests_cache.install_cache('fcc', backend='sqlite', allowable_methods=('GET', 'POST'))
+requests_cache.install_cache('fcc', backend='sqlite', allowable_methods=('GET', 'POST'), expire_after=10000000)
 
 baseUrl = "https://publicfiles.fcc.gov/api/"
 
-def callApi(reqUrl):
+def callApi(reqUrl, params=None):
     '''
         Holding this here in case we might want to do some rate limits or proxying to local DB
     '''
-    print(baseUrl + reqUrl + '.json')
-    response = requests.get(baseUrl + reqUrl + '.json')
+    if not params: suffix = '.json'
+    else: suffix = ''
+
+    response = requests.get(baseUrl + reqUrl + suffix, params=params)
     if response.status_code == 200:
         response = response.json()
-        if response['status'] == 'OK':
+        if response['status'] in ['OK', 'success']:
             return response
         else:
             # throw error
             #TODO HANDLE ERRORS
-            print(response)
             return None
     else:
         print(response.status_code)
 
-def checkComments(content):
-    '''
-    Check if object is valid JSON, also remove comments from the top of the file if any.
-    :param content:
-    :return:
-    '''
+def downloadFile(reqUrl, downloadPath=None):
+    if not downloadPath:
+        downloadPath = './downloads/'
+    file_path = os.path.basename(urlparse.urlparse(reqUrl).path)
+    return urlretrieve(baseUrl + reqUrl, downloadPath +file_path)
